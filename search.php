f2086bce0b0bcf71a11d02276af6d7dba8c0e039
shreya1997 <shreyaaynical@gmail.com>
2018-06-28 18:04:00 +0530

    Add files via upload

---
+++ search.php
@@ -0,0 +1,82 @@
+<html>
+<style type="text/css">
+.nav  
+{	width:100%;
+	height:90px;
+	position:absolute;
+	top:0; left:0; color:white; z-index:7; font-family:verdana; -webkit-box-shadow: 0 3px 8px rgba(0, 0, 0, .25);
+	background-color: #3d5b99;	
+}
+</style>
+<body background="y.jpg">
+	<div class="nav"><h1>VENDX</h1> </div>
+	
+
+<?php
+ 
+$con = mysqli_connect('127.0.0.1','root','');
+                    if(!$con)
+                    {
+                      echo "Not Connected to database";
+                    }
+                    if(!mysqli_select_db($con,'evending'))
+                    {
+                      echo "Unable to establish connection";
+                    }
+
+if (isset($_POST["search"])) 
+{
+    $searching = $_POST["search"];
+    $searching = preg_replace("#[^a-z]#i", "", $searching);
+	$sql = "SELECT vend FROM product WHERE name LIKE '%$searching%'" ;
+                    $result = $con-> query($sql);
+                   // $count=mysql_num_rows($result);
+                    $row = $result->fetch_assoc();
+                   
+                   /*if( $count == 0)
+                    {
+                    echo 'no such item available';
+                    }*/
+                    //else
+					
+                    {
+                       //while ($row)
+                        {
+						if ($row['vend'] == 1)
+							{
+								echo "<div style=\"text-align:center\">";
+								echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
+								echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
+								echo 'AVAILABLE IN '; echo '<h1><a href="drinks1.php" style="color: black;">VENDING MACHINE 1</a></h1>';
+								echo "</div>";
+							}
+						else if ($row['vend'] == 2)
+							{
+								echo "<div style=\"text-align:center\">";
+								echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
+								echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
+								echo 'AVAILABLE IN '; echo '<h1><a href="drinks2.php" style="color: black;">VENDING MACHINE 2</a></h1>';
+								echo "</div>";
+							}
+						else if ($row['vend'] == 3)
+							{
+								echo "<div style=\"text-align:center\">";
+								echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
+								echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
+								echo 'AVAILABLE IN '; echo '<h1><a href="drinks3.php" style="color: black;">VENDING MACHINE 3</a></h1>';
+								echo "</div>";
+							}
+						else
+							{
+								echo "<div style=\"text-align:center\">";
+								echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
+								echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
+								echo 'AVAILABLE IN '; echo '<h1><a href="drinks4.php" style="color: black;">VENDING MACHINE 4</a></h1>';
+								echo "</div>";
+							}
+                        }
+                    }
+}
+?>
+</body>
+</html>
